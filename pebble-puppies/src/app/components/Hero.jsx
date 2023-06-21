d482e260ba658b307abf005e07f173f4c68ee56f
Jondo <chet9880@gmail.com>
2023-06-21 13:49:59 -0400

    created hero image, need to possibly change how I implemented it image % isn't working how I want it to

--- pebble-puppies/src/app/components/Hero.jsx
+++ pebble-puppies/src/app/components/Hero.jsx
@@ -1,9 +1,22 @@
-import "../css/Hero.css";
+import styles from "../css/Hero.module.css";
+import { Roboto } from "next/font/google";
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 export default function Hero() {
   return (
     <div>
-      <img src="http://via.placeholder.com/1200x1500" />
+      <div className={styles.hero__img}>
+        <div className={`${styles.hero__text__container} ${roboto.className}`}>
+          <h2 className={styles.hero__h2}>
+            Discover the Joy of Pebble Puppies
+          </h2>
+          <h3 className={styles.hero__h3}>Welcome to Our Playful Universe!</h3>
+          <div className={styles.hero__button}>Buy Now</div>
+        </div>
+      </div>
     </div>
   );
 }
--- pebble-puppies/src/app/components/Nav.jsx
+++ pebble-puppies/src/app/components/Nav.jsx
@@ -1,4 +1,4 @@
-import "../css/Nav.css";
+import styles from "../css/Nav.module.css";
 import { Noto_Sans } from "next/font/google";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
@@ -10,22 +10,22 @@ const noto_sans = Noto_Sans({
 
 export default function Nav() {
   return (
-    <div className="container">
-      <div className="navbar">
-        <img src="./joyko.png" alt="Joyko Logo" />
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <img className={styles.logo} src="/joyko.png" alt="Joyko Logo" />
         <nav>
-          <ul className={noto_sans.className}>
-            <li>Home</li>
-            <li>Products</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
+          <ul className={`${noto_sans.className} ${styles.navlist}`}>
+            <li className={styles.nav__list__elements}>Home</li>
+            <li className={styles.nav__list__elements}>Products</li>
+            <li className={styles.nav__list__elements}>About Us</li>
+            <li className={styles.nav__list__elements}>Contact Us</li>
           </ul>
         </nav>
-        <div className="shopping--links">
-          <span className={`${noto_sans.className} register--button`}>
+        <div className={styles.shopping__links}>
+          <span className={`${noto_sans.className} ${styles.register__button}`}>
             Register Now
           </span>
-          <div className="shoppingcart">
+          <div className={styles.shoppingcart}>
             <FontAwesomeIcon icon={faCartShopping} color="#44BB9F" />
           </div>
         </div>
